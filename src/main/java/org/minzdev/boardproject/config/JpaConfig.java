6b0561c07599bc42e84480097cb7d62733d69443
ISMINIMIN <summmm.224@gmail.com>
2024-04-20 21:21:48 +0900

    fix: 엔티티 변환 및 JPA Auditing 설정

---
+++ src/main/java/org/minzdev/boardproject/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package org.minzdev.boardproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        // TODO: 스프링 시큐리티로 인증 기능을 붙이면 수정 예정
+        return () -> Optional.of("minzdev");
+    }
+}
